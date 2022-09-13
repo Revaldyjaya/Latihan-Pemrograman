196e88ec7001107efc615b11ced518c34abc5012
revaldyjaya051204@gmail.com <revaldyjaya051204@gmail.com>
2022-09-13 10:46:14 +0700

    tugas1

---
+++ Latihan 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+     cout<<"nama saya Revaldy";
+
+     return 0;
+ 
+}
---
+++ Pengulangan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    for (i = 1; i < 5; i++) 
+    {
+      cout << "Belajar Koding di Kelas Algoritma" << endl;
+    }
+
+    return 0;
+}
