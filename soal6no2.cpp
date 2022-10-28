5f0cab809a97266fe41c27cd9a5e1ea78c405c4b
Revaldyjaya <113406769+Revaldyjaya@users.noreply.github.com>
2022-10-28 18:46:03 +0700

    Add files via upload

---
+++ soal6no2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+
+char a[] = "KAMPUS UNGU INSTITUT BISNIS DAN INFORMATIKA KESATUAN";
+cout <<a<<endl;
+cout <<strlwr(a)<<endl;
+cout <<strrev(a);
+// Memakai VS Code. Pada Replit, strlwr dan strrev tidak bisa digunakan.
+
+return 0;
+}
